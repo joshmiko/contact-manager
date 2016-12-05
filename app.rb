371999a92c07c36dbd0cde79833d731cdc2da652
joshmiko <mikolaities.j@gmail.com>
2016-12-04 20:27:11 -0500

    Initial Commit

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require "sinatra/activerecord"
+
+require_relative 'models/contact'
+also_reload 'models/contact'
+
+set :bind, '0.0.0.0'  # bind to all interfaces
+
+get '/' do
+  @contacts = Contact.all
+  erb :index
+end
+
+get '/contacts/:id' do
+  @contact = Contact.all.find(params[:id])
+  erb :show
+end
